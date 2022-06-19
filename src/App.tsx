4b5c402988d3c93f24e0f811ac7b8ca07311515f
Vladimir <vladimir.yakimov.9811@gmail.com>
2022-06-19 16:19:32 +0300

    3rd microtask and changing README.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,35 @@
 import React, {useState} from 'react';
 import './App.css';
-import {NewComponent} from "./components/NewComponent/NewComponent";
+import {FullInput} from "./components/FullInput/FullInput";
 import {Button} from "./components/Button/Button";
-import {NewComponent1} from "./components/NewComponent1/NewComponent1";
-
-export type FilterType = 'Dollars' | 'RUBLS' | 'All'
-
-export type MoneyType = {
-    banknots: string
-    value: number
-    number: string
-}
+import {Input} from "./components/Input/Input";
 
 function App() {
-    const [money, setMoney] = useState<Array<MoneyType>>([
-        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    let [message, setMessage] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
     ])
 
-    const [filter, setFilter] = useState<FilterType>('All');
+    let [title, setTitle] = useState('');
 
-    let currentMoney = money;
-
-    if (filter === 'RUBLS') {
-        currentMoney = money.filter(filteredMoney => filteredMoney.banknots === 'RUBLS');
-    }
-    if (filter === 'Dollars') {
-        currentMoney = money.filter(filteredMoney => filteredMoney.banknots === 'Dollars');
-    }
-
-
-    const onClickFilterHandler = (nameButton: FilterType) => {
-        setFilter(nameButton);
+    const addMessage = (title:string) => {
+        let newMessage = {message: title}
+        setMessage([newMessage, ...message])
+        setTitle('')
     }
 
     return (
-        <div>
-            <NewComponent1
-                currentMoney={currentMoney}
-                onClickFilterHandler={onClickFilterHandler}
-            />
+        <div className={'App'}>
+            {/*<FullInput addMessage={addMessage}/>*/}
+            <Input setTitle={setTitle} title={title}/>
+            <Button name={'+'} callBack={()=>addMessage(title)}/>
+            {message.map((m, index) => {
+                return (
+                    <div key={index}>{m.message}</div>
+                )
+            })}
+
         </div>
     )
 }
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,17 @@
+import React, {ChangeEvent, useState} from 'react';
+
+type InputType = {
+    setTitle: (title: string) => void
+    title: string
+}
+
+export const Input = (props: InputType) => {
+
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        props.setTitle(event.currentTarget.value);
+    }
+
+    return (
+        <input value={props.title} onChange={onChangeInputHandler}/>
+    );
+}
